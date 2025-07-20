2cecd8f8c9ae0e24424ac3dc4dd205d9c0eef210
MakiforDevelop <makifordevelop@gmail.com>
2025-07-20 16:38:38 +0800

    Initial Commit

---
+++ NekoDupImgFinder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NekoDupImgFinder
+//
+//  Created by 千葉牧人 on 2025/7/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NekoDupImgFinder/NekoDupImgFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  NekoDupImgFinderApp.swift
+//  NekoDupImgFinder
+//
+//  Created by 千葉牧人 on 2025/7/20.
+//
+
+import SwiftUI
+
+@main
+struct NekoDupImgFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
